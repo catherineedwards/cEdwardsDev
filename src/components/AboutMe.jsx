0fbb87ade7dc1cec48cc08cebb1976cd7488e73c
catherineedwards <catherine@edwards-au.com>
2019-07-02 20:55:29 +1200

    First scaffold of React Site

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "../index.css";
+
+class AboutMe extends Component {
+  render() {
+    return (
+      <div className="aboutme">
+        <h1>
+          About Me
+        </h1>
+        <p>
+          Hello!
+        </p>
+        <p>
+          
+        </p>
+        <p>
+          
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AboutMe;
